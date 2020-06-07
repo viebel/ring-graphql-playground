7cfe7af53291af3e443bb7f84536997ad3910c32
Yehonathan Sharvit <viebel@gmail.com>
2020-06-07 20:43:55 +0300

    add conf.js example

--- resources/graphql-playground/conf.js
+++ resources/graphql-playground/conf.js
@@ -1,2 +1,20 @@
 // Default config. This file is not used when using ring-graphql-playground handler.
-window.GRAPHQL_PLAYGROUND_CONF = {endpoint: "/graphql"};
+window.GRAPHQL_PLAYGROUND_CONF = {
+    endpoint: 'https://eu1.prisma.sh/public-asdf/session65/dev',
+    tabs: [ {
+        query: `query Rules($bar: String, $foo: Int) {
+  rules {
+    id
+  }
+}
+query MyRules($bar: String, $foo: Int) {
+  rules {
+    id
+  }
+}
+
+`,
+        endpoint: 'https://eu1.prisma.sh/public-asdf/session65/dev',
+        variables: '{"foo": 3, "bar": "fooz"}'
+    }],
+}
